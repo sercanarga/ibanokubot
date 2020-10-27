79736c21f59d4f6c82d7b55088cec11a834c5349
sercanarga <mail@sercanarga.com>
2020-10-27 04:48:45 +0300

    Revert "git testi"
    This reverts commit ab2d5578

---
+++ src/telegram.php
@@ -0,0 +1,27 @@
+<?php
+
+#git test
+
+namespace sercanarga; class Telegram {
+    public $api_url, $api_key;
+
+	function __construct($settings) {
+		$this->api_url = $settings['api_url'];
+		$this->api_key = $settings['api_key'];
+	}
+
+	function request($method, $param) {
+		$params = http_build_query($param);
+		$url = "$this->api_url$this->api_key/$method?$params";
+        return file_get_contents($url);
+	}
+
+	function save_img($url) {
+        $dir = 'imgs/';
+        $path = $dir.basename($url);
+        file_put_contents($path, file_get_contents($url));
+        return $path;
+    }
+}
+
+?>
